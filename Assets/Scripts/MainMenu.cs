fc400f4e5d3676484e5989a3f1744e6da09a3b21
Joyce <99140907+atinyzen@users.noreply.github.com>
2023-02-11 22:06:22 +0100

    added some assets
    music assets & emoji sprites

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -17,6 +17,11 @@ public class MainMenu : MonoBehaviour
         
     }
 
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
     public void LoadCreditsScene()
     {
         SceneManager.LoadScene("Credits");
